c8706e29bc32957a9fd56826c4653c594b86def9
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-11 00:57:15 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -31,6 +31,7 @@ end #end get_first_name_of_season_winner
 
 def get_contestant_name(data, occupation)
   # Returns the name of the woman who has occupation
+  
 end
 
 def count_contestants_by_hometown(data, hometown)
